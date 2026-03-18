241bfbaaf214803367c45b69e36c9888a830925f
ASYA <asya-beyza.yakut@ens.uvsq.fr>
2026-03-18 21:56:17 +0100

    Update Pendu.py

--- .github/workflows/Pendu.py
+++ .github/workflows/Pendu.py
@@ -12,7 +12,7 @@ def indexs(lettre, caractere):
 la fonction cherche tous les indices d'une certaine lettre et les retourne sous forme d'une liste
 '''
 def remplacer(mot1,ncaractere,index):
- '''
+   '''
 insere le nouveau string "tranches" et l'original
  '''
  return mot1[:index]+ ncaractere+ mot1[index+1:]
@@ -41,7 +41,7 @@ def pendu():
       print("oops ressayer :(")
       print("le mot etait:"+ mot)
       break
-    '''
+      '''
   boucle avec une condition d'arret si le joueur a gagner ou si il lui reste plus d'essais 
    '''
     deviner=str(input("Mot : " + montrer + " | Essais restants : " + str(essais) + "\n"))
