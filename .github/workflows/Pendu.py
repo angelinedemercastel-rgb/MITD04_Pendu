89520d3c88ed4213dd2e43a7ed3fe05a949d61cf
Samy <88536172+Samyrahmani@users.noreply.github.com>
2026-03-18 21:23:25 +0100

    Update Pendu.py

--- .github/workflows/Pendu.py
+++ .github/workflows/Pendu.py
@@ -8,3 +8,7 @@ def indexs(lettre, caractere):
   [indexes.append(k) for k, name in enumerate(caractere) if name==lettre]
   return indexes
 #la fonction cherche tous les indices d'une certaine lettre et les retourne sous forme d'une liste
+
+def remplacer(mot1,ncaractere,index):
+  #insere le nouveau string "tranches" et l'original
+  return mot1[:index]+ ncaractere+ mot1[index+1:]
