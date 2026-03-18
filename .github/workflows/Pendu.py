db298aca3dab48446e22acb494fcf08e3aabd748
angelinedemercastel-rgb <angelinedemercastel@gmail.com>
2026-03-18 21:16:06 +0100

    Update Pendu.py

--- .github/workflows/Pendu.py
+++ .github/workflows/Pendu.py
@@ -2,3 +2,9 @@ import random
 
 Mots = ("python", "algorithme", "suite", "probleme", "etude",  "reel")
   #tuple temporaire de mots
+
+def indexs(lettre, caractere):
+  indexes=[]
+  [indexes.append(k) for k, name in enumerate(caractere) if name==lettre]
+  return indexes
+#la fonction cherche tous les indices d'une certaine lettre et les retourne sous forme d'une liste
