dd48a84f8dc07471a971757f2efd78d6fb8cdb50
ASYA <asya-beyza.yakut@ens.uvsq.fr>
2026-04-08 21:10:14 +0200

    Initialize Pendu CLI with basic functions
    Add initial structure and functions for Pendu CLI.

--- .github/workflows/pendu_cli.py
+++ .github/workflows/pendu_cli.py
@@ -1,1 +1,16 @@
+import random
 
+liste_mots = []
+
+cheminFichier = "./listeDeMots.txt"
+
+def afficheBonjour():
+    print("Bonjour ")
+
+def lireChoix():
+    print("\n***Bonjour***")
+    print("\n[J]ouer au Pendu")
+    print("[A]jouter un nouveau mot")
+    print("[Q]uitter")
+    choix = input("\nChoix : ")
+    return choix
