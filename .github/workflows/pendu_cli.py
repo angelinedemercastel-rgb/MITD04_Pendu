fbf75a9346076bf2db2af971c90754c5adc8d558
angelinedemercastel-rgb <angelinedemercastel@gmail.com>
2026-04-08 21:13:08 +0200

    Update pendu_cli.py

--- .github/workflows/pendu_cli.py
+++ .github/workflows/pendu_cli.py
@@ -14,3 +14,22 @@ def lireChoix():
     print("[Q]uitter")
     choix = input("\nChoix : ")
     return choix
+
+def lireListeMots(chemin) :
+    print("Chemin", chemin)
+    file=open(chemin, "r")
+    lignes = file.readlines()
+    for ligne in lignes :
+        ligne=ligne.strip()
+        if ligne :
+            parties = ligne.lower().split(",")
+            liste_mots.append(parties)
+    file.close()
+    return liste_mots
+
+def listeIndexLettre(lettre,mot):
+    lIndex = []
+    for i in range(0,len(mot)):
+        if lettre == mot[i]:
+            lIndex.append(i)
+    return lIndex
