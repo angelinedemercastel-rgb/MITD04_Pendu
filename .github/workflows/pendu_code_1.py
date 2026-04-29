97ab0c911088b240ee434a42b000020bde2f0f5a
Samy <88536172+Samyrahmani@users.noreply.github.com>
2026-04-29 21:38:15 +0200

    Update pendu_code_1.py

--- .github/workflows/pendu_code_1.py
+++ .github/workflows/pendu_code_1.py
@@ -1,2 +1,30 @@
+from tkinter import *
+import operations as op
 
+'''
+Fonction : rejouer
+Remet chaine vide les chaines de caracteres
+Réinitialise l'affichage
+Sélectionne un nouveau mot dans la liste
+'''
+def rejouer():
+    global score
+    lettreSaisie =""
+    mot_secret = ""
+    mot_a_deviner = ""
+    indice_mot = ""
+    razAffichage()
+    nouveauMot()
+    score=7
+'''
+Fonction : validerNouveauMot
+'''
+def validerNouveauMot():
+    mot=eMot.get()
+    #print(mot)
+    indice=eIndice.get()
+    #print(indice)
+    if len(mot)!=0 :
+        op.ajouterMotIHM(op.cheminFichier,mot,indice)
+    fenetreAddWord.destroy()
 
